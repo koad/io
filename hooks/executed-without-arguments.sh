9c776fe8410915e8e983945d2d41e198c8ae836d
koad on wonderland <wonderland@kingofalldata.com>
2022-04-13 15:01:49 -0400

    added non-args hook

---
+++ hooks/executed-without-arguments.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "koad:io entity ran without arguments, so,.. not gonna do anything."
+echo
+echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguements.sh"
+echo "have fun!"
