dc39ad5011b9db6e924b5899786b80654926d8c4
koad on wonderland <wonderland@kingofalldata.com>
2022-04-14 15:36:37 -0400

    fixed bad spelling

--- hooks/executed-without-arguments.sh
+++ hooks/executed-without-arguments.sh
@@ -2,5 +2,5 @@
 
 echo "koad:io entity ran without arguments, so,.. not gonna do anything."
 echo
-echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguements.sh"
+echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguments.sh"
 echo "have fun!"
