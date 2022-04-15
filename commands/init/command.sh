271a31c50271241ea54adc0182b287f8eb70e0c4
koad on wonderland <wonderland@kingofalldata.com>
2022-04-14 21:19:30 -0400

    cleaned up what is left from it's makeover.

--- commands/init/command.sh
+++ commands/init/command.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# TODO: if fold length is more than 80, make it 80
+# WORD_WRAP_WIDTH=$(tput cols)
+WORD_WRAP_WIDTH=80
+
+ENTITY=$1
+DATADIR=$HOME/.$ENTITY
+
 echo
 echo
 echo "koad:io 2016-2022 © kingofalldata.com"
@@ -12,14 +19,6 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit 1
 fi
-echo
-ENTITY=$1
-echo "$ENTITY,..?"
-
-
-WORD_WRAP_WIDTH=$(tput cols)
-DATADIR=$HOME/.$ENTITY
-# TODO: if fold length is more than 80, make it 80
 
 sleep 1
 [ -d $DATADIR ] && echo 'Directory already exists, cannot proceed.' && exit 1
@@ -41,11 +40,8 @@ mkdir -p $DATADIR/media   && [[ $DEBUG ]] && echo "[init] creating $DATADIR/medi
 mkdir -p $DATADIR/archive && [[ $DEBUG ]] && echo "[init] creating $DATADIR/archive"
 mkdir -p $DATADIR/keybase && [[ $DEBUG ]] && echo "[init] creating $DATADIR/keybase"
 
-[ ! -v $DEBUG ] && echo "[[ gestation output locations suppressed ]]"
-
-echo
-echo
-echo
+sleep 1
+[ ! -v $DEBUG ] && echo "[[ gestation output locations suppressed ]]" && echo
 
 sleep 1
 echo "Generating master elliptic curve parameters"
@@ -77,48 +73,46 @@ openssl genpkey -algorithm EC  -pass pass:$ENTITY -pkeyopt ec_paramgen_curve:P-2
 echo "generated: $DATADIR/ssl/session.pem"
 echo
 
-
 sleep 1
-echo && printf '%s\n' "Generating a key to be used during Diffie Hellman Key Exchanges.  This will help ensure that your new friend can identify herself easily when she sees herself.  It is also used when running secure dapps to ensure each dapp is correctly talking to the right friend before any handshaking begins." | fold -w $WORD_WRAP_WIDTH -s
-
-echo
+printf '%s\n' "Generating keys to be used during Diffie Hellman Key Exchanges." | fold -w $WORD_WRAP_WIDTH -s
+printf '%s\n' "This will help ensure that your new friend can identify herself easily when he sees itself across networks." | fold -w $WORD_WRAP_WIDTH -s
 echo "homework: research the Diffie Hellman key exchange process"
 echo
+
 sleep 1
 echo "Generating 2048 bit dhparam, this won't take so long, about a minute."
 openssl dhparam -out $DATADIR/ssl/dhparam-2048.pem 2048 > /dev/null 2>&1
 echo "generated: $DATADIR/ssl/dhparam-2048.pem 2048"
 echo
 echo "Generating 4096 bit dhparam, this will take a long time, 10 minutes max?"
-# openssl dhparam -out $DATADIR/ssl/dhparam-4096.pem 4096 > /dev/null 2>&1
+openssl dhparam -out $DATADIR/ssl/dhparam-4096.pem 4096 > /dev/null 2>&1
 echo "generated: $DATADIR/ssl/dhparam-4096.pem 4096"
 sleep 1
-
 
 echo
 echo "Keychain generated. Please secure your new friend by backing him up; NOW!"
 echo 
 echo "All files created for $ENTITY are saved in a single directory: '$HOME/.$ENTITY'"
-echo "back up this directory somewhere safe backup keychain".
-echo "back up the SSL directory twice, use an ESP and print a paper key"
+echo "back up this directory somewhere safe".
+echo "back up the SSL directory twice, use an ESP and print a paper key; this is $ENTITY's keychain"
 echo "bookmark https://book.koad/sh/reference/backup-your-entity for help"
 echo
+
 sleep 1
-
 echo "Creating entity wrapper command: $ENTITY"
-echo '#!/bin/bash
-export ENTITY='$ENTITY'
-koad-io "$@";' > $HOME/.koad-io/bin/$ENTITY
+echo '#!/usr/bin/env bash
 
-# echo "creating symbolic link from '$HOME/.koad-io/bin/entity' to '$HOME/.koad-io/bin/$ENTITY'"
-# ln -s $HOME/.koad-io/bin/entity $HOME/.koad-io/bin/$ENTITY
+export ENTITY="'$ENTITY'"
+koad-io "$@";
+' > $HOME/.koad-io/bin/$ENTITY
+echo
 
+sleep 1
 echo "making '$HOME/.koad-io/bin/$ENTITY' executable"
 chmod +x $HOME/.koad-io/bin/$ENTITY
-
 echo
-sleep 1
 
+sleep 1
 echo "archving command version information"
 git rev-parse --short HEAD > $DATADIR/VERSION
 [ -f $DATADIR/VERSION ] && cat $DATADIR/VERSION
@@ -126,16 +120,8 @@ echo "wrote version information to: $DATADIR/VERSION"
 echo
 
 sleep 1
-
 echo "...gestation complete!"
-sleep 1
 echo
-sleep 1
-echo
-sleep 1
-echo "ready player one -> $ENTITY"
-sleep 1
-
 
 # TODO: Create a sack of keys used to deal with issuing packages
 
@@ -144,37 +130,22 @@ sleep 1
 # TODO: spawn 2 nebula networks using this keyring
 #       one for machines
 #       one for humans
-echo
-sleep 1
+sleep 3
 echo "Congratulations!"
 echo
-# echo && printf '%s\n' "You've just created brand a new digital life!  Enjoy YOUR new digital best friend! You can keep this new friend private, or choose to make aspects of her public.  You are in control." | fold -w $WORD_WRAP_WIDTH -s
-# echo && printf '%s\n' "To help you keep yourself organized within your own mind, it is recommended to create a new digital friend for each one of the different focus areas of your life.  Focus areas generally involve different groups of people and activities." | fold -w $WORD_WRAP_WIDTH -s
-# echo && printf '%s\n' "An example of a focus area is,..  your band, your brand, your family, your business, your church, your team and hopefully even yourself.  You want to keep a solid barrier between the data from each area of your life; work and play." | fold -w $WORD_WRAP_WIDTH -s
-# echo && printf '%s\n' "You can create as many new friends as you wish, and teach them how to gather data and communicate with each-other to bring you the most personalized and private digital assistant experience known to man." | fold -w $WORD_WRAP_WIDTH -s
+echo && printf '%s\n' "You've just created brand a new digital life!  Enjoy YOUR new digital best friend! You can keep this new friend private, or choose to make aspects of her public.  You are in control." | fold -w $WORD_WRAP_WIDTH -s
 echo && printf '%s\n' "You can use any existing friend to create another." | fold -w $WORD_WRAP_WIDTH -s
 echo "ie: > $ENTITY init alice"
 echo
-echo
-echo
-# echo && printf '%s\n' "NOTICE:  This software is highly experimental, is self-hosted and is attempting to be cryptographically secure.  Be sure to understand each of these three things and take the design your backup and recovery plan.  Always keep a minimum of 3 devices (or more) active at one time;  Your data's safety increases with each trusted device you keep active." | fold -w $WORD_WRAP_WIDTH -s
-# echo && printf '%s\n' "WARNING:  If you lose access to all of your devices at one then YOU WILL BE LOCKED OUT and unable to recover your new friend, she will be gone forever.  Be sure to learn about cryptography.  It is risky to use cryptographic tools without knowing what they are and how they work." | fold -w $WORD_WRAP_WIDTH -s
-echo
-# echo && printf '%s\n' "If you find bugs, or have questions, please join us within our keybase channels -> https://keybase.io/team/canadaecoin" | fold -w $WORD_WRAP_WIDTH -s
-# echo && printf '%s\n' "Use any of the commands with the options -h or --help to find out more about each command." | fold -w $WORD_WRAP_WIDTH -s
-# echo "> $ENTITY --help"
-# echo && printf '%s\n' "You will need to either add your new friend to your path manually, or, close this terminal and open another." | fold -w $WORD_WRAP_WIDTH -s
-# echo '> export PATH=$PATH:'$DATADIR'/bin'
-echo ""
-# echo && printf '%s\n' "To start using $ENTITY, assign her some roles and responsibilities within the appropriate UI" | fold -w $WORD_WRAP_WIDTH -s
-# echo "cli: > $ENTITY enter control"
-# echo "gui: > $ENTITY show control"
 
-echo "try 'alice test'"
-echo " then try 'alice test one two three four'"
+echo "try '$ENTITY test'"
+echo " then try '$ENTITY test one two three four'"
+echo
 
-echo ""
 echo "have fun with $ENTITY!  I hope you make it into something nice."
-echo ""
+echo
 echo "/koad"
 
+sleep 3
+echo "ready player one -> $ENTITY"
+echo
