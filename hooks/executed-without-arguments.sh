1ddc35db329474f652cd5e8065e521f101b314e6
koad on wonderland <wonderland@kingofalldata.com>
2022-04-19 13:29:51 -0400

    reworked the non-argument hook to reload bash with new entity.

--- hooks/executed-without-arguments.sh
+++ hooks/executed-without-arguments.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-echo "koad:io entity ran without arguments, so,.. not gonna do anything."
-echo
-echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguments.sh"
-echo "have fun!"
+# echo "koad:io entity ran without arguments, so,.. not gonna do anything."
+# echo
+# echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguments.sh"
+# echo "have fun!"
+
+# Drop into a new bash prompt with the entity called forth.
+echo && echo && bash
+
+# When the user exists the previously launched terminal, reset the terminal window title.
+echo -ne "\033]0;${USER} on ${HOSTNAME}\007" && echo
