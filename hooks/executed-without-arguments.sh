ecb4cfeb9cfe2c7bb12e936f3ef1290ea20231a7
koad <wonderland@koad.sh>
2023-11-22 14:09:43 -0500

    Fixed environment passing issue

--- hooks/executed-without-arguments.sh
+++ hooks/executed-without-arguments.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+echo && echo 
 
-# echo "koad:io entity ran without arguments, so,.. not gonna do anything."
-# echo
-# echo "but! You can change this behaviour by updating ~/.koad-io/hooks/executed-without-arguments.sh"
-# echo "have fun!"
+# Check if dotenv-cli is installed
+if ! command -v dotenv &>/dev/null; then
+    echo "dotenv-cli is not installed. Please install it using npm:" >&2
+    echo "npm install -g dotenv-cli" >&2
+    exit 1
+fi
 
 # Drop into a new bash prompt with the entity called forth.
-echo && echo && bash
+dotenv -e ~/.$ENTITY/.env bash
 
 # When the user exists the previously launched terminal, reset the terminal window title.
 echo -ne "\033]0;${USER} on ${HOSTNAME}\007" && echo
