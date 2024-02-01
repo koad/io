361a40358d0795f602f738c4ebf23f3117721b40
koad on wonderland <wonderland@koad.sh>
2024-02-01 13:51:48 -0500

    added ability to select the fill char

--- packages/koad-io-core/both/initial.js
+++ packages/koad-io-core/both/initial.js
@@ -13,8 +13,9 @@ koad = {
 	storage: {},
 	library: {},
 	format: {
-		timestamp: function(d) {
-		if(!d) d = new Date();	
+		timestamp: function(d, s) {
+		if(!d) d = new Date();
+		if(!s) s = ":";
 		  const date = new Date(d);
 		  const year = date.getFullYear();
 		  const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -22,7 +23,7 @@ koad = {
 		  const hours = String(date.getHours()).padStart(2, '0');
 		  const minutes = String(date.getMinutes()).padStart(2, '0');
 		  const seconds = String(date.getSeconds()).padStart(2, '0');
-		  return `${year}:${month}:${day}:${hours}:${minutes}:${seconds}`;
+		  return `${year}${s}${month}${s}${day}${s}${hours}${s}${minutes}${s}${seconds}`;
 		}
 	},
 	seeders: [],
