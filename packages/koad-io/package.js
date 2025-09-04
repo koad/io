2e9c2a25974c86364a49bdbe43526c61ccb8e53e
Jason Zvaniga <git@koad.sh>
2025-09-04 14:13:01 -0400

    Update package.js
    removed template helpers dependancy

--- packages/koad-io/package.js
+++ packages/koad-io/package.js
@@ -19,7 +19,6 @@ Package.onUse(function(api) {
   api.imply("standard-minifier-css");
   api.imply("standard-minifier-js");
   api.imply("mobile-experience");
-  api.imply("koad:io-template-helpers");
   api.imply("koad:io-router");
   api.imply("koad:io-session");
   api.imply("koad:io-plus-head-js");
