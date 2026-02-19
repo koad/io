5845c9de87885e71dfaff819299b77a009ed6f56
koad <github@kingofalldata.com>
2026-02-19 14:48:38 -0500

    Update koad-io-theme-engine for Meteor 3.0 and add metadata

--- packages/koad-io-theme-engine/package.js
+++ packages/koad-io-theme-engine/package.js
@@ -1,30 +1,27 @@
 Package.describe({
   name: 'koad:io-theme-engine',
   version: '0.0.1',
-  summary: '',
-  git: '',
-  documentation: null
+  summary: 'Theme engine for koad:io Meteor applications',
+  git: 'https://github.com/koad/io-theme-engine',
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
+	api.versionsFrom(['3.0'])
+
 	api.use("koad:io-core", "client");
 	api.use("templating", "client");
 	api.use("tracker", "client");
 	api.use("reactive-var", "client");
 
 	api.addFiles([
-		'styles/01-normalize.css', // should be first on the list, so it cleans things before anything.
-		'styles/02-variables.css', // should be second on the list, so it is loaded before the others.
+		'styles/01-normalize.css',
+		'styles/02-variables.css',
 		'styles/body.css',
 		'styles/media-queries.css',
 	], 'client');
 
-	api.addFiles([		
+	api.addFiles([
 		'logic.js',
 	], 'client');
-
-});
-
-
-Package.onTest(function(api) {
 });
