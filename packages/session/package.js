202b0a8bc1b907ddbb4e2b50b5f572a7f11610b4
koad on wonderland <wonderland@koad.sh>
2026-03-17 20:42:32 -0400

    Add koad:io-session for persistent client-side storage
    Enables Meteor Session values to persist across page refreshes using
    browser localStorage, with support for temporary, persistent, and
    authenticated session types.

---
+++ packages/session/package.js
@@ -0,0 +1,33 @@
+Package.describe({
+  name: "koad:io-session",
+  version: "0.5.0",
+  summary: "Persistently store Session data on the client",
+  git: "https://github.com/koad/io-session"
+});
+
+Package.onUse(function(api) {
+  api.use(['jquery', 'tracker', 'reactive-dict', 'session', 'underscore', 'ejson']);
+  // If `accounts-base` is loaded, we have to make sure that this package is
+  // loaded after `accounts-base` is, so we specify `weak: true` here
+  api.use('accounts-base', { weak: true });
+
+  api.addFiles('lib/amplify.js', 'client');
+  api.addFiles('lib/persistent_session.js', 'client');
+  api.export('Session', ['client']);
+  api.export('PersistentSession', ['client']);
+});
+
+Package.onTest(function (api) {
+  api.use("tinytest");
+  api.use("random");
+  api.use("underscore");
+  api.use("reactive-dict"); // we only need this exposed for testing
+  api.use("koad:io-session");
+
+  // expose for derping around in console
+  api.export('Session', ['client']);
+  api.export('PersistentSession', ['client']);
+  api.export('ReactiveDict', ['client']);
+
+  api.addFiles("tests/client/persistent_session.js", "client");
+});
